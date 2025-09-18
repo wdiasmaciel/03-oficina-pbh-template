dfd70acdb1753d9613279eb52969bfdd2f504d86
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-18 04:59:25 +0000

    Exemplo

---
+++ login.js
@@ -0,0 +1,17 @@
+const email = usuario.email;
+const senha = usuario.senha;
+
+function verificarLogin() {
+    const emailInformado = document.getElementById("email").value;
+    const senhaInformada = document.getElementById("senha").value;
+
+    if (emailInformado === email) {
+        alert("E-mail informado corretamente!");
+        if (senhaInformada === senha) {
+            alert("Senha informada corretamente!");
+            window.location = "home.html";
+        } else
+            alert("Senha incorreta!");
+    } else
+        alert("E-mail incorreto!");
+}
